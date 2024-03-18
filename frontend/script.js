e78be935e1e27976471f59058f8062b0ede68144
Fernando Acarapi <130022317+FerFisX@users.noreply.github.com>
2024-03-17 20:27:46 -0400

    Funcionalidad script.js

--- frontend/script.js
+++ frontend/script.js
@@ -2,6 +2,7 @@ const todoContainer = document.querySelector(".todo-container");
 const inputTodo = document.getElementById("input-todo");
 const inputDescrip = document.getElementById("input-descrip");
 const inputFecha = document.getElementById("input-fecha");
+const inputTiempoEstimado = document.getElementById("input-tiempo-estimado");
 const addTodo = document.getElementById("add-todo");
 
 const modalBG = document.querySelector(".modal-background");
@@ -9,9 +10,9 @@ const closeModal = document.querySelector("#close-modal");
 const editTodoTitulo = document.getElementById("edit-todo-titulo");
 const editTodoDescrip = document.getElementById("edit-todo-descrip");
 const editTodoFecha = document.getElementById("edit-todo-fecha");
+const editTodoTiempoEstimado = document.getElementById("edit-todo-tiempo-estimado");
 const editTodoCompleted = document.getElementById("edit-todo-completed");
 const saveTodo = document.getElementById("save-todo");
-
 
 const searchInput = document.getElementById("search-input");
 const searchButton = document.getElementById("search-button");
@@ -20,56 +21,56 @@ let todoArray = [];
 
 const URL = "http://localhost:3005/todos";
 
-
 async function get_Todos() {
-  try {
-    const resp = await fetch(URL);
-    const data = await resp.json();
-    return data;
-  } catch (err) {
-    return err;
-  }
+    try {
+        const resp = await fetch(URL);
+        const data = await resp.json();
+        return data;
+    } catch (err) {
+        return err;
+    }
 }
 
 async function post_Todo() {
     try {
-      let options = {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          titulo: inputTodo.value,
-          descripcion: inputDescrip.value,
-          fecha: inputFecha.value,
-          completed: false,
-        }),
-      };
-      const resp = await fetch(URL, options);
-      const data = await resp.json();
-      retur
+        let options = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                titulo: inputTodo.value,
+                descripcion: inputDescrip.value,
+                fecha: inputFecha.value,
+                tiempo_estimado: inputTiempoEstimado.value, // Incluir tiempo estimado en la solicitud
+                completed: false,
+            }),
+        };
+        const resp = await fetch(URL, options);
+        const data = await resp.json();
+        return data;
     } catch (err) {
-      return err;
+        return err;
     }
 }
 
 async function del_Todo(todoElem) {
     try {
-      const del_url = URL + "/" + todoElem._id;
-      let options = {
-        method: "DELETE",
-      };
-      const resp = await fetch(del_url, options);
-      const data = await resp.json();
-      return data;
+        const del_url = URL + "/" + todoElem._id;
+        let options = {
+            method: "DELETE",
+        };
+        const resp = await fetch(del_url, options);
+        const data = await resp.json();
+        return data;
     } catch (err) {
-      return err;
+        return err;
     }
 }
 
 async function edit_Todo(todoElem) {
     try {
-        let edit_url = URL + "/" + todoElem._id; 
+        let edit_url = URL + "/" + todoElem._id;
         let options = {
             method: "PUT",
             headers: {
@@ -79,6 +80,7 @@ async function edit_Todo(todoElem) {
                 titulo: editTodoTitulo.value,
                 descripcion: editTodoDescrip.value,
                 fecha: editTodoFecha.value,
+                tiempo_estimado: editTodoTiempoEstimado.value, // Incluir tiempo estimado en la solicitud
                 completed: editTodoCompleted.checked,
             }),
         };
@@ -90,87 +92,84 @@ async function edit_Todo(todoElem) {
     }
 }
 
-
 function open_modal(todoElem) {
-    editTodoTitulo.value= todoElem.titulo;
-    editTodoDescrip.value=todoElem.descripcion;
+    editTodoTitulo.value = todoElem.titulo;
+    editTodoDescrip.value = todoElem.descripcion;
     editTodoFecha.value = todoElem.fecha;
+    editTodoTiempoEstimado.value = todoElem.tiempo_estimado; // Mostrar tiempo estimado en el modal
     editTodoCompleted.checked = todoElem.completed;
     modalBG.style.display = "block";
     closeModal.addEventListener("click", () => {
-      modalBG.style.display = "none";
+        modalBG.style.display = "none";
     });
     saveTodo.addEventListener("click", () => {
-      modalBG.style.display = "none";
-      edit_Todo(todoElem);
+        modalBG.style.display = "none";
+        edit_Todo(todoElem);
     });
-    console.log(todoElem);
-  }
-
+}
 
 function display_Todos(todoArr) {
     todoArr.forEach((todoElem) => {
-        console.log(todoElem);
-        // Parent
-    let todo = document.createElement("div");
-    todo.classList.add("todo");
+        // Crear elementos HTML para mostrar la información de cada tarea
+        let todo = document.createElement("div");
+        todo.classList.add("todo");
 
-    // Children
-    let todoInfo = document.createElement("div");
-    todoInfo.classList.add("todo-info");
-    let todoBtn = document.createElement("form");
-    todoBtn.classList.add("todo-btn");
+        let todoInfo = document.createElement("div");
+        todoInfo.classList.add("todo-info");
+        let todoBtn = document.createElement("form");
+        todoBtn.classList.add("todo-btn");
 
+        let todoName = document.createElement("p");
+        todoName.classList.add("todo-titulo");
+        todoName.innerHTML = todoElem.titulo;
 
-    let todoName = document.createElement("p");
-    todoName.classList.add("todo-titulo");
-    todoName.innerHTML = todoElem.titulo;
+        let todoDescripcion = document.createElement("p");
+        todoDescripcion.classList.add("todo-descrip");
+        todoDescripcion.innerHTML = todoElem.descripcion;
 
+        let todoFecha = document.createElement("p");
+        todoFecha.classList.add("todo-fecha");
+        todoFecha.innerHTML = todoElem.fecha;
 
-    let todoDescripcion = document.createElement("p");
-    todoDescripcion.classList.add("todo-descrip");
-    todoDescripcion.innerHTML = todoElem.descripcion;
+        let todoTiempoEstimado = document.createElement("p"); // Nuevo elemento para mostrar tiempo estimado
+        todoTiempoEstimado.classList.add("todo-tiempo-estimado");
+        todoTiempoEstimado.innerHTML = `Tiempo estimado: ${todoElem.tiempo_estimado} minutos`;
 
-    let todoFecha = document.createElement("p");
-    todoFecha.classList.add("todo-fecha");
-    todoFecha.innerHTML = todoElem.fecha;
+        let todoCompleted = document.createElement("input");
+        todoCompleted.classList.add("todo-completed");
+        todoCompleted.setAttribute("type", "checkbox");
+        todoCompleted.checked = todoElem.completed;
 
-    // Grand Children
-    let todoCompleted = document.createElement("input");
-    todoCompleted.classList.add("todo-completed");
-    todoCompleted.setAttribute("type", "checkbox");
-    todoCompleted.checked = todoElem.completed;
+        let todoEdit = document.createElement("button");
+        todoEdit.classList.add("todo-edit");
+        todoEdit.innerHTML = "Editar";
+        todoEdit.addEventListener("click", (e) => {
+            e.preventDefault();
+            open_modal(todoElem);
+        });
+        let todoDel = document.createElement("button");
+        todoDel.classList.add("todo-delete");
+        todoDel.innerHTML = "Eliminar";
+        todoDel.addEventListener("click", () => {
+            del_Todo(todoElem);
+        });
 
-    let todoEdit = document.createElement("button");
-    todoEdit.classList.add("todo-edit");
-    todoEdit.innerHTML = "Editar";
-    todoEdit.addEventListener("click", (e) => {
-      e.preventDefault();
-      console.log("Open modal")
-      open_modal(todoElem);
+        todoInfo.appendChild(todoCompleted);
+        todoInfo.appendChild(todoName);
+        todoInfo.appendChild(todoDescripcion);
+        todoInfo.appendChild(todoFecha);
+        todoInfo.appendChild(todoTiempoEstimado); // Agregar tiempo estimado al contenedor
+        todoBtn.appendChild(todoEdit);
+        todoBtn.appendChild(todoDel);
+
+        todo.appendChild(todoInfo);
+        todo.appendChild(todoBtn);
+
+        todoContainer.appendChild(todo);
     });
-    let todoDel = document.createElement("button");
-    todoDel.classList.add("todo-delete");
-    todoDel.innerHTML = "Eliminar";
-    todoDel.addEventListener("click", () => {
-      console.log(todoElem);
-      del_Todo(todoElem);
-    });
-    todoInfo.appendChild(todoCompleted);
-    todoInfo.appendChild(todoName);
-    todoInfo.appendChild(todoDescripcion);
-    todoInfo.appendChild(todoFecha);
-    todoBtn.appendChild(todoEdit);
-    todoBtn.appendChild(todoDel);
-
-    todo.appendChild(todoInfo);
-    todo.appendChild(todoBtn);
-
-    todoContainer.appendChild(todo);
-});
 }
 
-// Función para filtrar las notas según la búsqueda
+// Función para filtrar las tareas según la búsqueda
 function filterTodos(searchTerm) {
     return todoArray.filter(todo => {
         const titleMatch = todo.titulo.toLowerCase().includes(searchTerm.toLowerCase());
@@ -185,11 +184,11 @@ searchButton.addEventListener("click", () => {
     const searchTerm = searchInput.value.trim();
     if (searchTerm !== "") {
         const filteredTodos = filterTodos(searchTerm);
-        todoContainer.innerHTML = ""; 
+        todoContainer.innerHTML = "";
         display_Todos(filteredTodos);
     } else {
-        todoContainer.innerHTML = ""; 
-        display_Todos(todoArray); 
+        todoContainer.innerHTML = "";
+        display_Todos(todoArray);
     }
 });
 
@@ -200,28 +199,27 @@ searchInput.addEventListener("keypress", event => {
     }
 });
 
-
+// Obtener tareas al cargar la página
 get_Todos()
     .then(todoArr => {
         todoArray = todoArr;
-        console.log(todoArray);
         display_Todos(todoArray);
     })
     .catch((err) => console.log(err));
 
-    addTodo.addEventListener("click", async () => {
-        if (inputTodo.value !== "" && inputDescrip.value !== "" && inputFecha.value !== "") {
-            await post_Todo(); 
-            
-            const updatedTodos = await get_Todos();
-            todoArray = updatedTodos;
-            todoContainer.innerHTML = ""; 
-            display_Todos(todoArray); 
-          
-            inputTodo.value = "";
-            inputDescrip.value = "";
-            inputFecha.value = "";
-        }
-    });
-    
-      
+// Event listener para agregar nueva tarea
+addTodo.addEventListener("click", async () => {
+    if (inputTodo.value !== "" && inputDescrip.value !== "" && inputFecha.value !== "" && inputTiempoEstimado.value !== "") {
+        await post_Todo();
+
+        const updatedTodos = await get_Todos();
+        todoArray = updatedTodos;
+        todoContainer.innerHTML = "";
+        display_Todos(todoArray);
+
+        inputTodo.value = "";
+        inputDescrip.value = "";
+        inputFecha.value = "";
+        inputTiempoEstimado.value = "";
+    }
+});
